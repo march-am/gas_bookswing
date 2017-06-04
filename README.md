9e853d7024c1b3665f18d9d99d75a29b366e1443
march-am <myredblood@gmail.com>
2017-06-04 20:20:59 +0900

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Google App Script for BooksWing
+
+## 説明
+
+1. [BooksWing](http://www.bookswing.com/)で読書した本のメモを溜める
+1. 読書データをメールで自分のGmailアドレスに送る（「メールで送ること」）
+1. 受信したメールに'*bookswing*'という名前のラベルを付ける（抽出クエリはよしなに）
+1. GASで実行するとパースしたObjectが返ってくる
+
+## フォーマット
+
+```javascript
+books = [
+  {
+    author: String('author'),
+     title: String('title'),
+     posts: {
+      quote: String('引用'),
+       memo: String('メモ'),
+       page: Number('ページ数')
+    }
+  },
+  //...
+]
+```
--- dev/main.js
+++ dev/main.js
@@ -2,26 +2,15 @@
 import { getMailsWithArray, parseBWMailBody } from './parse_mails';
 
 global.main = () => {
-  const label_name = 'bookswing';
-  const mails = getMailsWithArray('label:' + label_name);
-  const mail = mails[0];
+  const mails = getMailsWithArray('label:bookswing');
   const books = mails.map((mail) => {
     const [title, author] = mail.subject.split(/ - /);
-    const body = parseBWMailBody(mail.body);
+    const posts = parseBWMailBody(mail.body);
     return {
       author : author,
       title  : title,
-      body   : body
+      posts  : posts
     }
   });
   Logger.log(books);
-
-  // posts = [
-  //   {
-  //      quote: String('引用'),
-  //       memo: String('メモ'),
-  //       page: Number('ページ数')
-  //   },
-  //   ...
-  // ]
 };
